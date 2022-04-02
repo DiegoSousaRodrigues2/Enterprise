dc1443535faeca4178410304c57151b0f0dbd267
DiegoSousaRodrigues2 <rm87910@fiap.com.br>
2022-04-02 19:06:02 -0300

    Inicio

---
+++ 05-jpa-design-patterns/src/main/java/br/com/fiap/jpa/entity/Cliente.java
@@ -0,0 +1,67 @@
+package br.com.fiap.jpa.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CLIENTE")
+@SequenceGenerator(name="cliente",sequenceName = "SQ_TB_CLIENTE",allocationSize = 1)
+public class Cliente {
+
+	@Id
+	@Column(name="cd_cliente")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cliente")
+	private Integer codigo;
+	
+	@Column(name="nm_cliente", nullable = false, length = 80)
+	private String nome;
+	
+	@Column(name="dt_nascimento")
+	private LocalDate dataNascimento;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="ds_genero", length = 20)
+	private Genero genero;
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+}
